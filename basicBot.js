948d7f202ec73af895d2730ff585871495e0d292
Jamie Duke <jamiedukeellis@users.noreply.github.com>
2014-12-24 11:27:52 +1100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1748,13 +1748,39 @@
             jamieCommand: {
                 command: 'jamie',
                 rank: 'user',
-                type: 'exact',
+                type: 'startsWith',
+                cookies: ['Hai Der!',
+                    'Hai Der!',
+                    
+                ],
+                getCookie: function () {
+                    var c = Math.floor(Math.random() * this.cookies.length);
+                    return this.cookies[c];
+                },
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        var link = "Hai der!";
-                        API.sendChat(subChat(basicBot.chat.starterhelp, {link: link}));
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
                     }
                 }
             },
