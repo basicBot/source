138961626a8abbf355fbc1b02b59d10086257ba4
robotoid <jamesvcrisman2@gmail.com>
2014-12-29 19:58:36 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1848,7 +1848,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var link = "http://i.imgur.com/SBAso1N.jpg";
-                        API.sendChat(subChat('Roll:', math.floor(math.random())));
+                        API.sendChat(subChat('Roll:', {rand_val;} = ())));
                     }
                 }
             },
