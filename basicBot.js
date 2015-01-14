1e64266e071d9377b47bec64ab5af94614ff06dc
RatedRion <ratedrion@gmail.com>
2015-01-14 02:13:14 -0500

    Revert "langindex too?"
    This reverts commit 69216007c498cd7a088cafc22b6fd1a3aa83ed89.

--- basicBot.js
+++ basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/ServePeak/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
