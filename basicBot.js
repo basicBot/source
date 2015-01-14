36707f34a31ea55d31f72a7e78504bb79a6b2b5f
RatedRion <ratedrion@gmail.com>
2015-01-14 02:13:18 -0500

    Revert "fix en.json link"
    This reverts commit f5376c3c5a3e117d6fa0890851f662661fc5926a.

--- basicBot.js
+++ basicBot.js
@@ -184,7 +184,7 @@
         loggedInID: null,
         scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/ServePeak/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
