2ff37207703a57da735c578d3f34b4d5d34a9a32
Benzi Mooneegan <benzi@email.com>
2015-01-19 16:32:12 +0000

    Added !blinfo command.
    Use !blinfo to get information required to blacklist a song.

--- basicBot.js
+++ basicBot.js
@@ -1503,6 +1503,26 @@
                 }
             },
 
+            blinfoCommand: {
+                command: 'blinfo',
+                rank: 'bouncer',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var author = API.getMedia().author;
+                        var title = API.getMedia().title;
+                        var name = chat.un;
+                        var format = API.getMedia().format;
+                        var cid = API.getMedia().cid;
+                        var songid = format + ":" + cid;
+
+                        API.sendChat(subChat(basicBot.chat.blinfo, {name: name, author: author, title: title, songid: songid}));
+                    }
+                }
+            },
+
             bouncerPlusCommand: {
                 command: 'bouncer+',
                 rank: 'mod',
