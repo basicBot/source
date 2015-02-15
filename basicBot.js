bda8a34eb0d65f4093201ea9138bfd39113123df
robotoid <jamesvcrisman2@gmail.com>
2015-02-14 22:09:36 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2257,7 +2257,7 @@
 
             maxlengthCommand: {
                 command: 'maxlength',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
