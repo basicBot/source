169298776eb0f30bfac8afa236241602bc1eae3e
Benzi <hi@bnzi.uk>
2015-03-11 17:39:56 +0000

    Fixed typo.
    Fixed voteskip related typo.

--- basicBot.js
+++ basicBot.js
@@ -1787,7 +1787,7 @@
                             API.sendChat(subChat(basicBot.chat.toggleoff, {name: chat.un, 'function': basicBot.chat.voteskip}));
                         }
                         else {
-                            basicBot.settings.voteskip = !basicBot.settings.voteskip;
+                            basicBot.settings.voteSkip = !basicBot.settings.voteSkip;
                             API.sendChat(subChat(basicBot.chat.toggleon, {name: chat.un, 'function': basicBot.chat.voteskip}));
                         }
                     }
@@ -2691,7 +2691,7 @@
                         msg += '. ';
 
                         msg += basicBot.chat.voteskip + ': ';
-                        if (basicBot.settings.voteskip) msg += 'ON';
+                        if (basicBot.settings.voteSkip) msg += 'ON';
                         else msg += 'OFF';
                         msg += '. ';
 
