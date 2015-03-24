7fc37722f8c028cb54793b4371bd41c7f7f0f805
Benzi <hi@bnzi.uk>
2015-03-24 19:11:59 +0000

    Changed historySkip to false.
    Set historyskip to false as default.

--- basicBot.js
+++ basicBot.js
@@ -209,7 +209,7 @@
             maximumCycletime: 10,
             voteSkip: false,
             voteSkipLimit: 10,
-            historySkip: true,
+            historySkip: false,
             timeGuard: true,
             maximumSongLength: 10,
             autodisable: true,
