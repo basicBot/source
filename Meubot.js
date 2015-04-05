00a8bcb38b43ca718729c28357ae71bc696e8e8e
Klindson <klindson@live.com>
2015-04-05 07:02:18 -0300

    Rename basicBot.js to Meubot.js

---
+++ Meubot.js
@@ -0,0 +1,123 @@
+(function () {
+    
+    var fork = "Klindson";
+		
+		
+    function extend() {
+        
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+
+        var bot = window.bot;
+        
+        bot.retrieveSettings();
+
+        bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
+                    'deu-lhe um biscoito de pinto caseiro macio!',
+                    'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
+                    'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
+                    'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
+                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador e coma um negão!"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
+                    'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
+                    'deu-lhe um rabanete e um copo de leite.',
+                    'deu-lhe um biscoito de arco-íris feito com amor :heart:',
+                    'deu-lhe um biscoito que fio esquecido na chuva... eu não comeria.',
+                    'te trouxe biscoitos fresquinhos... parecem deliciosos!'
+                ];
+                bot.commands.cookieCommand = {
+            command: 'cookie',
+            rank: 'ambassador',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    
+                }
+            }
+        };
+        
+            bot.commands.pingCommand = {
+            command: 'ping',
+            rank: 'mod',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    
+                }
+            }
+        };
+        
+        bot.loadChat();
+    }
+        localStorage.setItem("basicBotsettings", JSON.stringify({
+        botName: "Olá,bem vindo infeliz e volte sempre arrombado :point_right: :ok_hand:",
+        language: "portuguese",
+        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
+        startupCap: 4,
+        startupVolume: 50,
+        startupEmoji: true,
+        maximumAfk: 60,
+        afkRemoval: true,
+        maximumDc: 60,
+        bouncerPlus: true,
+        lockdownEnabled: false,
+        lockGuard: true,
+        maximumLocktime: 10,
+        cycleGuard: true,
+        maximumCycletime: 10,
+        voteSkip: true,
+        voteSkipLimit: 10,
+        timeGuard: true,
+        maximumSongLength: 8.30,
+        autodisable: true,
+        commandCooldown: 30,
+        usercommandsEnabled: true,
+        lockskipPosition: 3,
+        lockskipReasons: [
+                ["tema", "A música não se encaixa nos padrões da sala. "],
+                ["op", "Essa música está na lista OP. "],
+                ["historico", "A música ainda está no histórico. "],
+                ["mix", "Você tocou um mix (muito longo) - não permitido. "],
+                ["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
+                ["nsfw", "A música que você tocou é NSFW (impróprio). "],
+                ["indisponivel", "A música que você tocou está indisponível. "]
+            ],
+        afkpositionCheck: 15,
+        afkRankCheck: "ambassador",
+        motdEnabled: true,
+        motdInterval: 8,
+        motd: "!roulette",
+        filterChat: true,
+        etaRestriction: true,
+        welcome: true,
+        opLink: null,
+        rulesLink: null,
+        themeLink: "http://i.imgur.com/u36VR4n.png,"
+        fbLink: "null",
+        youtubeLink: null,
+        website: "null",
+        intervalMessages: [],
+        messageInterval: 5,
+        songstats: false,
+        commandLiteral: "!","/"
+        blacklists: {
+            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
+        }
+    }));
+
+    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+
+}).call(this);
