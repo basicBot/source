0a78f2fb993b1c271905c8bdf1274ac12afffa48
VladasX <vladukas.m@gmail.com>
2015-06-19 02:14:00 +0300

    Update

--- basicBot.js
+++ basicBot.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -241,7 +241,7 @@
         name: "basicBot",
         loggedInID: null,
         scriptLink: "https://rawgit.com/VladasX/basicBot/master/basicBot.js",
-        cmdLink: "http://git.io/245Ppg",
+        cmdLink: "https://github.com/VladasX/basicBot/blob/master/lang/langIndex.json",
         chatLink: "https://rawgit.com/VladasX/basicBot/master/lang/lt.json",
         chat: null,
         loadChat: loadChat,
