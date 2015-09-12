82b3576609cc4283908c870f3604eb8f72091b4e
Benzi <hi@benzi.io>
2015-09-12 20:22:59 +0100

    v2.8.15

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "2.8.14",
+        version: "2.8.15",
         status: false,
         name: "basicBot",
         loggedInID: null,
