2582dae7e37048086a30fce882c1f4144443cf11
javascript <info@leakoni.net>
2016-06-06 00:09:48 +0200

    Update basicBot-test.js

--- basicBot-test.js
+++ basicBot-test.js
@@ -963,7 +963,7 @@
             if (typeof lastplay === 'undefined') return;
             if (basicBot.settings.songstats) {
                 if (typeof basicBot.chat.songstatistics === "undefined") {
-                    API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + "W/" + lastplay.score.grabs + "G/" + lastplay.score.negative + "M.")
+                    API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + ":thumbsup:/" + lastplay.score.grabs + ":green_heart:/" + lastplay.score.negative + ":thumbsdown:.")
                 }
                 else {
                     API.sendChat(subChat(basicBot.chat.songstatistics, {artist: lastplay.media.author, title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
