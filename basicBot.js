d0cf0e954f30f1f8c6557252c5f047410a65d650
javascript <info@leakoni.net>
2016-06-05 21:08:07 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3089,7 +3089,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me This bot was created by ' + botCreator + ', maintained by ' + botMaintainer + "," ' and now developed by ' + botEditor + ".");
+                        API.sendChat('/me This bot was created by ' + botCreator + ', but is now maintained by ' + botMaintainer + ".");
                     }
                 }
             },
