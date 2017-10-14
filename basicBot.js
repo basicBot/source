905e370d6976251ec6f984f46854902e221d6924
Lattvien <32798873+Lattvien@users.noreply.github.com>
2017-10-14 23:36:06 +0300

    Update basicbot.js
    Please? Its annoying to change it back to these setting every time I run this

--- basicBot.js
+++ basicBot.js
@@ -263,9 +263,9 @@
             chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
             scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
-            startupCap: 1, // 1-200
-            startupVolume: 0, // 0-100
-            startupEmoji: false, // true or false
+            startupCap: 200, // 1-200
+            startupVolume: 75, // 0-100
+            startupEmoji: true, // true or false
             autowoot: true,
             autoskip: false,
             smartSkip: true,
