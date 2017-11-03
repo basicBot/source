e6fbcda8ba45c3d9d6e80a3deac7ae3ac827d587
Benzi Mooneegan <hi@benzi.io>
2017-11-03 17:04:49 +0000

    Low global rank bug fix
    + Low global ranks with staff roles can now run the bot without being confused for a normal user
    + Also removed plug.dj's roles backwards compatibilty

--- basicBot.js
+++ basicBot.js
@@ -484,13 +484,11 @@
                 else u = API.getUser(obj);
                 if (botCreatorIDs.indexOf(u.id) > -1) return 9999;
 
-                if (u.gRole == 0) return u.role;
+                if (u.gRole < 3000) return u.role;
                 else {
                     switch (u.gRole) {
-                        case 3:
                         case 3000:
                             return (1*(API.ROLE.HOST-API.ROLE.COHOST))+API.ROLE.HOST;
-                        case 5:
                         case 5000:
                             return (2*(API.ROLE.HOST-API.ROLE.COHOST))+API.ROLE.HOST;
                     }
@@ -4154,9 +4152,9 @@
                                     var rank = 'Host';
                                 }
 
-                                if ([3, 3000].indexOf(rawrank.gRole) > -1) {
+                                if (rawrank.gRole == 3000) {
                                     var rank = 'Brand Ambassador';
-                                } else if ([5, 5000].indexOf(rawrank.gRole) > -1) {
+                                } else if (rawrank.gRole == 5000) {
                                     var rank = 'Admin';
                                 }
 
