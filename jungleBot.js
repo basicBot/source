9b686bc964d8fe736a793869e32c66f39396a261
Harry McKenzie <42568513+HarryMcKenzie@users.noreply.github.com>
2018-08-31 15:52:15 +0200

    hmm

--- jungleBot.js
+++ jungleBot.js
@@ -1612,7 +1612,7 @@
                                 if (!jungleBot.commands.executable(this.rank, chat)) return void(0);
                                 else {
                                     var msg = chat.message;
-                                    var lastplay = API.getHistory()[0];
+                                    var lastplay = API.getHistory()[1];
                                     if (typeof lastplay === 'undefined') return;
                                     var list;
                                     if (msg.length === cmd.length) list = 'BANNED';
@@ -1688,25 +1688,6 @@
                              }
                           }
                       },
-
-            // say
-
-            	sayCommand: {
-                        command: ['say'],
-                        rank: 'manager',
-                        type: 'startsWith',
-                        functionality: function(chat, cmd) {
-          
-                  				var msg = chat.message;
-                  				var cmdmsg = msg.substr(cmd.length + 1);
-          
-                            if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
-                            if (!jungleBot.commands.executable(this.rank, chat)) return void(0);
-                            else {
-                                       API.sendChat(cmdmsg);
-                            }
-                        }
-                    },
 
             // chu say brug?
 
