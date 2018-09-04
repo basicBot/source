858782de410316ab636222af8a236dcdaefce520
zEjoxx <41965704+zEjoxx@users.noreply.github.com>
2018-09-04 22:40:05 +0300

    Also this

--- jungleBot.js
+++ jungleBot.js
@@ -1898,6 +1898,28 @@
                               }
                           }
                       },
+		
+            // @user with dab
+
+          	dabCommand: {
+                          command: ['dab', 'xqcdab'],
+                          rank: 'residentdj',
+                          type: 'startsWith',
+                          functionality: function(chat, cmd) {
+
+          					var msg = chat.message;
+          					var cmdmsg = msg.substr(cmd.length + 1);
+
+                              if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                              if (!jungleBot.commands.executable(this.rank, chat)) return void(0);
+                              else {
+
+          						API.sendChat(subChat(jungleBot.chat.dab, {
+                                      name: cmdmsg,
+                                  }));
+                              }
+                          }
+                      },
 
           	//(◕‿◕✿) CHAT IS RUNNING IN POSITIVE CHAT OR BAN MODE (◕‿◕✿)
 
