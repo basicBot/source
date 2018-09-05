1a5da3143788c5c02f0d638c75bac9ad3298970d
zEjoxx <41965704+zEjoxx@users.noreply.github.com>
2018-09-05 13:54:04 +0300

    Fixed :)

--- jungleBot.js
+++ jungleBot.js
@@ -1634,7 +1634,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!jungleBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        API.sendChat('/me @' + chat.un + ' is now afk.');
+                        API.sendChat('/me @' + chat.un + ' is now AFK :residentsleeper: ');
       		          }
       		      }
       	    },
