c1c7f59f4d9eb5290c4dcdd38a2ad903b88c98aa
Harry McKenzie <42568513+HarryMcKenzie@users.noreply.github.com>
2018-09-07 03:13:05 +0200

    s p a c e

--- jungleBot.js
+++ jungleBot.js
@@ -1813,7 +1813,7 @@
                     var id = jungleBot.userUtilities.getID(name);
 
                     if (id) {
-                      API.sendChat('/me @' + chat.un + 'Specified user\'s  ID is "' + id + '".');
+                      API.sendChat('/me @' + chat.un + ' Specified user\'s  ID is "' + id + '".');
                     }
                     else {
                       API.sendChat('/me @' + chat.un + ' Invalid user specified.');
