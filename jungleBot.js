dba91a725d2ccd688738c00e8f6d354a76aa77eb
Harry McKenzie <42568513+HarryMcKenzie@users.noreply.github.com>
2018-09-07 03:25:46 +0200

    xqcBan

--- jungleBot.js
+++ jungleBot.js
@@ -1602,6 +1602,31 @@
             },
             */
 
+            banCommand: {
+                           command: 'ban',
+                           rank: 'bouncer',
+                           type: 'startsWith',
+                           functionality: function(chat, cmd) {
+                               if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                               if (!basicBot.commands.executable(this.rank, chat)) return void(0);
+                               else {
+                                   var msg = chat.message;
+                                   if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {
+                                       name: chat.un
+                                   }));
+                                   var name = msg.substr(cmd.length + 2);
+                                   var user = basicBot.userUtilities.lookupUserName(name);
+                                   if (typeof user === 'boolean') return API.sendChat(subChat(basicBot.chat.invaliduserspecified, {
+                                       name: chat.un
+                                   }));
+                                   var permFrom = basicBot.userUtilities.getPermission(chat.uid);
+                                   var permUser = basicBot.userUtilities.getPermission(user.id);
+                                   if (permUser >= permFrom) return void(0);
+                                   API.moderateBanUser(user.id, 1, API.BAN.PERMA);
+                               }
+                           }
+                       },
+
         //Infinite PogChamp works
 
                   pogcycleCommand: {
